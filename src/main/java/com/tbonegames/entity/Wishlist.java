0b8c8642c7692f50021e96ed74abc8cbff20baf2
Tyler Balaskovitz <tylerbalaskovitz@yahoo.com>
2023-05-12 19:55:21 -0400

    Begun to implement all of the entities to the program. Still need to fill out other layers of the application

--- src/main/java/com/tbonegames/entity/Wishlist.java
+++ src/main/java/com/tbonegames/entity/Wishlist.java
@@ -1,5 +1,25 @@
 package com.tbonegames.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "wishlist_table")
 public class Wishlist {
+	
+	@Id
+	@Column(length = 5)
+	private String wish_id;
+	
+	@Column(length = 50)
+	private String diplay_name;
+	
+	@Column(length = 50)
+	private String short_desc;
+	
+	@Column(length = 20)
+	private String category;
 
 }

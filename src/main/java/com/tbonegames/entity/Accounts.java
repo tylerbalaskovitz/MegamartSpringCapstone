0bf2f9b213dc1442bec9ba06189c24132eb910bd
Tyler Balaskovitz <tylerbalaskovitz@yahoo.com>
2023-05-16 21:09:19 -0400

    Begun to add a Many To Many relationship to the tables using Spring @ManyToMany annotation and the @JoinTable annotation

--- src/main/java/com/tbonegames/entity/Accounts.java
+++ src/main/java/com/tbonegames/entity/Accounts.java
@@ -1,8 +1,16 @@
 package com.tbonegames.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -53,6 +61,13 @@ public class Accounts {
 	
 	@Column(length = 15)
 	private String email;
+	
+	@ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@JoinTable(
+			name = "wishlist_table",
+			joinColumns = {@JoinColumn(name = "wish_id", referencedColumnName="ID")},
+			inverseJoinColumns= {@JoinColumn(name="name", referencedColumnName="ID")})
+	private List<Accounts> accounts = new ArrayList<>();
 	
 	
 
